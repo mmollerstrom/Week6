6770e3e33ca00576454fb8406693acc1e331fa55
claes <claesca@outlook.com>
2017-05-08 13:48:54 +0200

    add author to make tests working

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Article, type: :model do
     it { is_expected.to have_db_column :id }
     it { is_expected.to have_db_column :title }
     it { is_expected.to have_db_column :content }
+    it { is_expected.to have_db_column :author }
   end
 
   describe 'Validations' do
